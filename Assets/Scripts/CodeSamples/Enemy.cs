67419bda58acea0a66e8a44461f587296b30dd1d
Simon <simon.papandreou@gmail.com>
2025-07-06 17:08:52 +0200

    Add simple code sample.

---
+++ Assets/Scripts/CodeSamples/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CodeSamples
+{
+    public class Enemy : MonoBehaviour
+    {
+        public Vector3 direction;
+        public float speed;
+        private void Update()
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/CodeSamples/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace CodeSamples
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        public Enemy enemyPrefab;
+        
+        public int enemyCount;
+        public float minSpeed;
+        public float maxSpeed;
+
+
+        private void Start()
+        {
+            var gridSize = Mathf.CeilToInt(Mathf.Sqrt(enemyCount));
+            var offset = (gridSize - 1) / 0.5f;
+            
+            for (var i = 0; i < enemyCount; i++)
+            {
+                var x = i % gridSize;
+                var y = i / gridSize;
+                var direction = Random.insideUnitCircle.normalized;
+                var position = new Vector3(x - offset, 0, y - offset);
+                var enemy = Instantiate(enemyPrefab, position,  Quaternion.identity);
+                enemy.direction = new Vector3(direction.x, 0, direction.y);
+                enemy.speed = Random.Range(minSpeed, maxSpeed);
+            }
+        }
+    }
+}
